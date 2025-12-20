a3f9ca5ab1d4bc496553c880ad6133b96ae301c1
moussa-li <794926191@qq.com>
2025-12-20 12:46:54 +0800

    add mesh data

--- RenderEngine/Core/Definites.hpp
+++ RenderEngine/Core/Definites.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
+#include "Common/Vector.hpp"
+
 namespace EgLab
 {
     using DeltaTime = float;
 
     using IdType = unsigned int;
+
+    using CoordType = Vector3f;
+
+    using TexCoordType = Vector2f;
+
+    using IdxType = unsigned int;
+
 } // namespace EgLab
--- RenderEngine/Core/RenderMesh.hpp
+++ RenderEngine/Core/RenderMesh.hpp
@@ -2,16 +2,28 @@
 
 #include "Common/DynamicArray.hpp"
 #include "Common/Vector.hpp"
+#include "Definites.hpp"
 
 namespace EgLab
 {
+
     class RenderMesh
     {
+    public:
+        RenderMesh()
+        {
+        }
+
+        RenderMesh(DynamicArray<CoordType> vertices, DynamicArray<CoordType> normals,
+                   DynamicArray<TexCoordType> texCoords)
+        {
+        }
+
     private:
-        DynamicArray<Vector4f> _vertices;
-        DynamicArray<Vector4f> _normals;
-        DynamicArray<Vector4f> _texCoords;
-        DynamicArray<size_t> _indices;
+        DynamicArray<CoordType> _vertices;
+        DynamicArray<CoordType> _normals;
+        DynamicArray<TexCoordType> _texCoords;
+        DynamicArray<IdxType> _indices;
         // DynamicArray<Texture*> _textures;
     };
 
